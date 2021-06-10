c322ebc5a817f910cfe19b1c81c9f6c9907d071f
Lawrence N. Masullo <lmasullo@gmail.com>
2021-06-10 08:48:16 -0500

    Initial

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<header>Welcome!</header>
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+</nav>
+
+<!-- When a page loads from the routes directory it first loads everything in __layout.svelte and then puts the pages html in this slot -->
+<main>
+	<slot />
+</main>
+
+<footer>Copyright 2021</footer>
